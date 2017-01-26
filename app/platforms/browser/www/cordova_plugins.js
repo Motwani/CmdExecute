df4df50cbe96653a6748ed3de785a41b3d9fd282
Sanket Markan <sanketmarkan@gmail.com>
2017-01-26 09:45:26 +0530

    Speak Recoginition for android

--- app/platforms/browser/www/cordova_plugins.js
+++ app/platforms/browser/www/cordova_plugins.js
@@ -1,9 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-speechrecognizer/SpeechRecognizer.js",
+        "id": "cordova-plugin-speechrecognizer.SpeechRecognizer",
+        "pluginId": "cordova-plugin-speechrecognizer",
+        "clobbers": [
+            "plugins.speechrecognizer"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.1"
+    "cordova-plugin-whitelist": "1.3.1",
+    "cordova-plugin-speechrecognizer": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
--- app/www/js/speech.js
+++ app/www/js/speech.js
@@ -1,8 +1,19 @@
-function record()
+function record2()
 {
 	var recognition = new webkitSpeechRecognition();
     recognition.onresult = function(event) { 
       console.log(event.results[0][0].transcript) 
     }
     recognition.start();
+}
+
+function record() {
+	var maxMatches = 1;
+	var promptString = "Speak now"; // optional
+	var language = "en-US";         // optional
+	window.plugins.speechrecognizer.startRecognize(function(result){
+		alert(result);
+	}, function(errorMessage){
+		console.log("Error message: " + errorMessage);
+	}, maxMatches, promptString, language);
 }
